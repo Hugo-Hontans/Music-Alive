da1bbe32a6f68fad656f0b56eca9d66b0a2a1ada
chelgoui shems <shems.chelgoui@gmail.com>
2018-10-10 11:46:47 +0200

    updated structure and added bootstrap jquery font awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,21 +6,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'easy-musique';
-}
-
-
-const path="https://api.songkick.com/api/3.0/search/artists.json?apikey=";
-const pathcomp="&query="
-const apiKey="ZHKVrD2Nr66ufY7q";
-let artist="";
-let acces="";
-function getEvents(){
-  artist = (<HTMLInputElement>document.getElementById("artist")).value;
-  acces = path + apiKey + pathcomp + artist;
-  fetch(acces)
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById("venue").innerHTML = data.resultPage.results.artist.uri;
-      })
-  .catch(error => console.error(error))
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CartemapComponent } from './cartemap/cartemap.component';
 import { EventlistComponent } from './eventlist/eventlist.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartemapComponent,
     EventlistComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    SearchbarComponent,
   ],
   imports: [
     BrowserModule
@@ -18,19 +26,3 @@ import { EventlistComponent } from './eventlist/eventlist.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-const path="https://api.songkick.com/api/3.0/search/artists.json?apikey=";
-const pathcomp="&query="
-const apiKey="ZHKVrD2Nr66ufY7q";
-let artist="";
-let acces="";
-function getEvents(){
-  artist = (<HTMLInputElement>document.getElementById("artist")).value;
-  acces = path + apiKey + pathcomp + artist;
-  fetch(acces)
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById("venue").innerHTML = data.resultPage.results.artist.uri;
-      })
-  .catch(error => console.error(error))
-}
