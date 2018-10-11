732bd82ed3aa291c9085297d7fbb58bdf92519cb
chelgoui shems <shems.chelgoui@gmail.com>
2018-10-11 15:03:02 +0200

    starting api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Http, Response} from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable()
 export class AppComponent {
   title = 'easy-musique';
+  private apiUrl = 'https://api.songkick.com/api/3.0/search/artists.json?apikey=jGIjCGirpGWFCCqb&query=';
+  apiCall(form: NgForm) {
+    const artist: string = form.value['artist'];
+    console.log(artist);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CartemapComponent } from './cartemap/cartemap.component';
 import { EventlistComponent } from './eventlist/eventlist.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
-import { SearchbarComponent } from './searchbar/searchbar.component';
-
+import {HttpModule} from '@angular/http';
 @NgModule({
   declarations: [
     AppComponent,
     CartemapComponent,
     EventlistComponent,
-    HeaderComponent,
     FooterComponent,
     ContentComponent,
-    SearchbarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
