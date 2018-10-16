194c64e6361e3ca23b1fc42dec38574de8587647
Eliott R <eliott.raillere@gmail.com>
2018-10-16 14:12:47 +0200

    ajout carte+marqueurs statiques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { EventItemComponent } from './event-item/event-item.component';
 import { Input } from '@angular/core';
+
+
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,13 +21,17 @@ import { Input } from '@angular/core';
     EventlistComponent,
     FooterComponent,
     ContentComponent,
-    EventItemComponent
+    EventItemComponent,
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBNiGHB5tcAsl9ilspi0aqb093Nlvdy5L8'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cartemap/cartemap.component.ts
+++ src/app/cartemap/cartemap.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';;
 
-import OlMap from 'ol/Map';
-import OlXYZ from 'ol/source/XYZ';
-import OlTileLayer from 'ol/layer/Tile';
-import OlView from 'ol/View';
-import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'app-cartemap',
@@ -12,30 +7,12 @@ import { fromLonLat } from 'ol/proj';
   styleUrls: ['./cartemap.component.css']
 })
 export class CartemapComponent implements OnInit {
+  //donner des positions; les variables sont à lire dans "[longitude]" et "[latitude]" 
+  //dans l'HTML afin de donner des positions sur la carte
+  lat: number = 45.750000;
+  lng: number = 4.850000;
 
-  map: OlMap;
-  source: OlXYZ;
-  layer: OlTileLayer;
-  view: OlView;
+  ngOnInit(){
 
-  ngOnInit() {
-    this.source = new OlXYZ({
-      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
-    });
-
-    this.layer = new OlTileLayer({
-      source: this.source
-    });
-
-    this.view = new OlView({
-      center: fromLonLat([6.661594, 50.433237]),
-      zoom: 3
-    });
-
-    this.map = new OlMap({
-      target: 'map',
-      layers: [this.layer],
-      view: this.view
-    });
   }
 }
