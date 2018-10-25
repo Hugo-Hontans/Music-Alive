4dc7962fd2284b6c9eea70aa485854160c57addd
chelgoui shems <shems.chelgoui@gmail.com>
2018-10-25 14:38:20 +0200

    shusF

--- src/app/venue-page/venue-page.component.ts
+++ src/app/venue-page/venue-page.component.ts
@@ -1,6 +1,11 @@
+<<<<<<< Updated upstream
 import { SongkickService } from './../services/songkick.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+=======
+import { Component, OnInit, Input } from '@angular/core';
+
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-venue-page',
@@ -8,10 +13,11 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./venue-page.component.css']
 })
 export class VenuePageComponent implements OnInit {
-
+  @Input() objetTest;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.objetTest);
   }
 
 }
