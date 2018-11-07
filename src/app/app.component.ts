5abbb5ebfb26b9280a125cd572e5435f0c54348a
chelgoui shems <shems.chelgoui@gmail.com>
2018-11-07 10:55:49 +0100

    header change color and stick to the top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, Injectable, OnInit} from '@angular/core';
+import { HostListener, Inject, Component, Injectable, OnInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { SongkickService } from './services/songkick.service';
 import { Router } from '@angular/router';
+import { DOCUMENT } from '@angular/platform-browser';
 import { LocationService } from './services/location.service';
 @Component({
   selector: 'app-root',
@@ -18,57 +19,67 @@ export class AppComponent implements OnInit {
   type;
   myLng: number;
   myLat: number;
-  geoLoc=false;
+  geoLoc = false;
   locationGeo;
   objetLoc;
-  show=false;
+  show = false;
 
-  constructor(private service: SongkickService, private router: Router, private locService:LocationService) {
+  constructor(private service: SongkickService, private router: Router, private locService: LocationService,  ) {
     this.type = 'artist';
   }
 
   scrollToElement($element): void {
-    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    $element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
   }
 
   search(form: NgForm) {
     switch (form.value['type']) {
       case 'artist':
-        let artist: string = form.value['searchValue'];
+        const artist: string = form.value['searchValue'];
         this.router.navigate(['artists/' + artist]);
         break;
 
       case 'venue':
-        let venue: string = form.value['searchValue'];
+        const venue: string = form.value['searchValue'];
         this.router.navigate(['venues/' + venue]);
         break;
 
       case 'location':
-        let location: string = form.value['searchValue'];
+        const location: string = form.value['searchValue'];
         this.router.navigate(['locations/' + location]);
         break;
 
       default:
-        let error: string = form.value['searchValue'];
+        const error: string = form.value['searchValue'];
         this.router.navigate(['artists/' + error]);
         break;
     }
 
   }
+   @HostListener('window:scroll', [])
+   onWindowScroll() {
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 255) {
+      (document.querySelector('.toc') as HTMLElement).style.backgroundColor = 'grey';
+    } else if (number < 255) {
+      (document.querySelector('.toc') as HTMLElement).style.backgroundColor = 'transparent';
+    }
+  }
+
 
   displayLocationInfo = (position) => {
     this.myLng = position.coords.longitude;
     this.myLat = position.coords.latitude;
-    this.geoLoc=true;
+    this.geoLoc = true;
   }
 
   getGeolocalisation() {
     this.locService
-      .apiCallGeoloc(this.myLat,this.myLng)
+      .apiCallGeoloc(this.myLat, this.myLng)
       .subscribe((res: any) => {
-        this.objetLoc=res.resultsPage.results.location[0].metroArea.id
+        this.objetLoc = res.resultsPage.results.location[0].metroArea.id;
         this.show = true;
-        this.router.navigate(['locationcontent/'+this.objetLoc])
+        this.router.navigate(['locationcontent/' + this.objetLoc]);
       });
   }
 
@@ -77,12 +88,14 @@ export class AppComponent implements OnInit {
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
-    }    
-  }
-  ngDoCheck(){
-    if (this.geoLoc==true){
-    this.getGeolocalisation()
-    this.geoLoc=false;
     }
   }
+  ngDoCheck() {
+    if (this.geoLoc === true) {
+    this.getGeolocalisation();
+    this.geoLoc = false;
+    }
+  }
+
+
 }
