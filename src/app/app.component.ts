461ce4de06efcb9d0aafed2888287c10b0b872dc
chelgoui shems <shems.chelgoui@gmail.com>
2018-11-08 19:44:21 +0100

    added some style changed background colors to gray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   animations: [
     trigger('scrolledNotScrolled', [
       state('scrolled', style({
-        backgroundColor: 'grey',
+        backgroundColor: '#263238',
         boxShadow: '0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px 0px rgba(0, 0, 0, 0.14), 0px 1px 18px 0px rgba(0, 0, 0, 0.12)'
       })),
       state('notscrolled', style({
